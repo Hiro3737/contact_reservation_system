52238048b379ac0a10cfaa8d0745ed304f08f881
Hiro3737 <your.hiroki10037@gmail.com>
2018-11-12 01:30:46 +0000

    gravatar_for ヘルパーに追加_テスト前

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,13 @@
 module UsersHelper
 
+
+    def gravatar_for(user, options = { size: 50 })
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
+    
   # 在社時間計上用
   def working_hours(a,b)
     timein = Time.mktime(a.year, a.month, a.day, a.hour, a.min, 0, 0)
