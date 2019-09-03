862c2d35eab2b7ddfd536d1933863c3ac31c4638
Hiro3737 <hiroki10037@gmail.com>
2019-09-03 22:13:57 +0900

    fix:不要ファイル削除

--- db/migrate/20180520003520_add_shozoku_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddShozokuToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :shozoku, :string
-  end
-end
--- db/migrate/20180715090207_add_attendances_to_attendances.rb
+++
@@ -1,5 +0,0 @@
-class AddAttendancesToAttendances < ActiveRecord::Migration[5.0]
-  def change
-    add_column :attendances, :user_id, :integer
-  end
-end
