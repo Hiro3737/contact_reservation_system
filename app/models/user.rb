2a5ec9e101d6f92a8e48fbd81967ce6f0d00c62b
Hiro3737 <hiroki10037@gmail.com>
2019-09-03 22:14:26 +0900

    fix:migrate後

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,23 @@
 class User < ApplicationRecord
-  has_many :attendances
-  
   attr_accessor :remember_token, :activation_token, :reset_token
   attr_accessor :remember_token, :activation_token
-  before_save   :downcase_email
+  before_save :downcase_email
   before_create :create_activation_digest
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-   
-   validates :basic_work_time, presence: true
-   
-   validates :specified_work_time, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+      BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-    
+
   # ランダムなトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
@@ -35,56 +29,56 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-   # トークンがダイジェストと一致したらtrueを返す
+  # トークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
-  
-    # ユーザーのログイン情報を破棄する
+
+  # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
   end
-  
-  
-    # アカウントを有効にする
+
+  # アカウントを有効にする
   def activate
-    update_attribute(:activated,    true)
+    update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
   end
-  
-    # 有効化用のメールを送信する
+
+  # 有効化用のメールを送信する
   #def send_activation_email
-   # UserMailer.account_activation(self).deliver_now
+  # UserMailer.account_activation(self).deliver_now
   #end
-  
-    # パスワード再設定の属性を設定する
+
+  # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
-  
-    # パスワード再設定のメールを送信する
+
+  # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
-  
-    # パスワード再設定の期限が切れている場合はtrueを返す
+
+  # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
 
-    private
-      # メールアドレスをすべて小文字にする
-    def downcase_email
-      self.email = email.downcase
-    end
+  private
 
-    # 有効化トークンとダイジェストを作成および代入する
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
