72e211329ed8303e566bb875f9a23ad949c259ed
Hiro3737 <hiroki10037@gmail.com>
2019-09-03 22:20:32 +0900

    fix:不要なアクションとルートを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,75 +1,20 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:destroy, :basic_info_edit, :basic_info ,:index]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy, :basic_info_edit, :basic_info, :index]
 
   def index
-    
     @users = User.paginate(page: params[:page])
-
   end
-  
-  def show
-    
 
+  def show
     if current_user.admin?
       @user = User.find(params[:id])
     else
       @user = current_user
     end
-    # 曜日表示用に使用する
-    @youbi = %w[日 月 火 水 木 金 土]
-    
-    #基本情報
-    @basic_info = BasicInfo.find_by(id: 1)
-    
-    # 既に表示月があれば、表示月を取得する
-    if !params[:first_day].nil?
-      @first_day = Date.parse(params[:first_day])
-    else
-      # 表示月が無ければ、今月分を表示
-      @first_day = Date.new(Date.today.year, Date.today.month, 1)
-    end
-    #最終日を取得する
-    @last_day = @first_day.end_of_month
-
-    # 今月の初日から最終日の期間分を取得
-    (@first_day..@last_day).each do |date|
-      # 該当日付のデータがないなら作成する
-      #(例)user1に対して、今月の初日から最終日の値を取得する
-      if !@user.attendances.any? {|attendance| attendance.day == date }
-        linked_attendance = Attendance.create(user_id: @user.id, day: date)
-        linked_attendance.save
-      end
-    end
-    # 表示期間の勤怠データを日付順にソートして取得 show.html.erb、 <% @attendances.each do |attendance| %>からの情報
-    @attendances = @user.attendances.where('day >= ? and day <= ?', @first_day, @last_day).order("day ASC")
-  end
-  
-  
-  def basic_info
-    if params[:id].nil?
-      @user  = User.find(current_user.id)
-    else
-      @user  = User.find(params[:id])
-    end
   end
 
-  def basic_info_edit
-    
-    @user  = User.find(params[:id])
-    
-    if @user.update_attributes(user_params)
-      flash[:success] = "基本情報を更新しました。"
-      redirect_to @user
-    else
-      redirect_to @user
-    end
-  end
-  
-  
-  
-  
   def new
     @user = User.new
   end
@@ -77,20 +22,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    #  @user.send_activation_email
-     # flash[:info] = "あなたのアカウントをアクティブにするためメールしました。確認してください"
-    #  redirect_to root_url
-      log_in@user
-      flash[:success]="ようこそ"
+      #  @user.send_activation_email
+      # flash[:info] = "あなたのアカウントをアクティブにするためメールしました。確認してください"
+      #  redirect_to root_url
+      log_in @user
+      flash[:success] = "ようこそ"
       redirect_to @user
     else
-      render 'new'
+      render "new"
     end
   end
 
   def edit
     @user = User.find_by(id: params[:id])
-    
   end
 
   def update
@@ -98,44 +42,41 @@ class UsersController < ApplicationController
       flash[:success] = "プロフィールが更新されました"
       redirect_to @user
     else
-      render 'edit'
+      render "edit"
     end
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "ユーザーを削除しました"
     redirect_to users_url
   end
-  
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,:shozoku,
-                                   :basic_work_time, :specified_work_time, :password_confirmation)
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # beforeアクション
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
     end
-    
-    # beforeアクション
-    
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-         store_location
-        flash[:danger] = "ログインしてください"
-        redirect_to login_url
-      end
-    end
-    
-        # 正しいユーザーかどうか確認
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-    
-    # 管理者かどうか確認
-    def admin_user
-     redirect_to(root_url) unless current_user.admin?
-    end
-    
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,19 @@
 Rails.application.routes.draw do
-  root                'static_pages#home'
-  get    'help'    => 'static_pages#help'
-  get    'about'   => 'static_pages#about'
-  get    'contact' => 'static_pages#contact'
-  get    'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  get    'show'    => 'users#show'
-  delete 'logout'  => 'sessions#destroy'
-   # 出勤画面表示・編集
-  get  '/attendance_update', to: 'attendances#attendance_update'
-  post '/attendance_update', to: 'attendances#attendance_update'
-  patch '/attendance_update', to: 'attendances#attendance_update'
-
-
-  get  '/attendance_edit', to: 'attendances#attendance_edit'
-  post '/attendance_edit', to: 'attendances#attendance_edit'
-  patch '/attendance_edit', to: 'attendances#attendance_edit'
-
-  get    '/basic_info',    to: 'users#basic_info'
-  post   '/basic_info_edit',    to: 'users#basic_info_edit'
-  
-   get '/basic_info',      to: 'users#edit_basic_info'
-  post'/basic_info_edit', to: 'users#basic_info_edit'
-  
-  post  '/attendance_update_all', to: 'attendances#attendance_update_all'
-  get   '/attendance_update_all', to: 'attendances#attendance_update_all'
-  
-
-  
+  root "static_pages#home"
+  get "help" => "static_pages#help"
+  get "about" => "static_pages#about"
+  get "contact" => "static_pages#contact"
+  get "signup" => "users#new"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  get "show" => "users#show"
+  delete "logout" => "sessions#destroy"
 
   resources :users do
-    member do
-      get :following, :followers
-    end
   end
-  
-  resources :basic_infos
-  
+
   resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
   resources :users
 end
