95af48e37eb13e94f7a92b3cfccba7f8beb32b58
yusuke-takada00 <yu.neiho@gmail.com>
2019-12-01 16:22:59 +0000

    header

--- Gemfile
+++ Gemfile
@@ -14,8 +14,12 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'rails-i18n'
+<<<<<<< HEAD
 gem 'fullcalendar-rails'
 gem 'momentjs-rails'
+=======
+gem 'font-awesome-rails'
+>>>>>>> header
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20191110064822) do
 
   create_table "bookings", force: :cascade do |t|
@@ -47,6 +48,9 @@ ActiveRecord::Schema.define(version: 20191110064822) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+=======
+ActiveRecord::Schema.define(version: 20191130063455) do
+>>>>>>> header
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
